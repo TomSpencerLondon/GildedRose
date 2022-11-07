93301ea65af10a89c7279c583742a330e03f0221
Tom Spencer <thomas.spencer@codurance.com>
2022-11-07 19:49:52 +0000

    Simplify backstage passes

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -39,20 +39,18 @@ public class GildedRose {
     }
 
     if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-      if (("Aged Brie".equals(item.getName())) || "Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-        if (item.getQuality() < 50) {
-          item.setQuality(item.getQuality() + 1);
+      if (item.getQuality() < 50) {
+        item.setQuality(item.getQuality() + 1);
 
-          if (item.getSellIn() < 11) {
-            if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
-            }
+        if (item.getSellIn() < 11) {
+          if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
           }
+        }
 
-          if (item.getSellIn() < 6) {
-            if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
-            }
+        if (item.getSellIn() < 6) {
+          if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
           }
         }
       }
@@ -63,7 +61,7 @@ public class GildedRose {
         item.setQuality(0);
       }
     } else {
-      if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+      if ((!"Aged Brie".equals(item.getName()))) {
         if (item.getQuality() > 0) {
           item.setQuality(item.getQuality() - 1);
         }
