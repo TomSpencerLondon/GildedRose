1f7d528bba6e9510fc8f207ceffb00d0b8035c5c
Tom Spencer <thomas.spencer@codurance.com>
2022-11-07 19:36:49 +0000

    Raise conditional with if else surround

--- src/main/java/GildedRose.java
+++ src/main/java/GildedRose.java
@@ -38,44 +38,88 @@ public class GildedRose {
       return;
     }
 
-    if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-      if (item.getQuality() > 0) {
-        item.setQuality(item.getQuality() - 1);
-      }
-    } else {
-      if (item.getQuality() < 50) {
-        item.setQuality(item.getQuality() + 1);
-
-        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-          if (item.getSellIn() < 11) {
-            if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
-            }
-          }
-
-          if (item.getSellIn() < 6) {
-            if (item.getQuality() < 50) {
-              item.setQuality(item.getQuality() + 1);
-            }
-          }
-        }
-      }
-    }
-
-    item.setSellIn(item.getSellIn() - 1);
-
-    if (item.getSellIn() < 0) {
-      if (!"Aged Brie".equals(item.getName())) {
-        if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
-          if (item.getQuality() > 0) {
-            item.setQuality(item.getQuality() - 1);
-          }
-        } else {
-          item.setQuality(0);
+    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+      if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+        if (item.getQuality() > 0) {
+          item.setQuality(item.getQuality() - 1);
         }
       } else {
         if (item.getQuality() < 50) {
           item.setQuality(item.getQuality() + 1);
+
+          if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            if (item.getSellIn() < 11) {
+              if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+              }
+            }
+
+            if (item.getSellIn() < 6) {
+              if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+              }
+            }
+          }
+        }
+      }
+
+      item.setSellIn(item.getSellIn() - 1);
+
+      if (item.getSellIn() < 0) {
+        if (!"Aged Brie".equals(item.getName())) {
+          if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            if (item.getQuality() > 0) {
+              item.setQuality(item.getQuality() - 1);
+            }
+          } else {
+            item.setQuality(0);
+          }
+        } else {
+          if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+          }
+        }
+      }
+    } else {
+      if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+        if (item.getQuality() > 0) {
+          item.setQuality(item.getQuality() - 1);
+        }
+      } else {
+        if (item.getQuality() < 50) {
+          item.setQuality(item.getQuality() + 1);
+
+          if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            if (item.getSellIn() < 11) {
+              if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+              }
+            }
+
+            if (item.getSellIn() < 6) {
+              if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+              }
+            }
+          }
+        }
+      }
+
+      item.setSellIn(item.getSellIn() - 1);
+
+      if (item.getSellIn() < 0) {
+        if (!"Aged Brie".equals(item.getName())) {
+          if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            if (item.getQuality() > 0) {
+              item.setQuality(item.getQuality() - 1);
+            }
+          } else {
+            item.setQuality(0);
+          }
+        } else {
+          if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+          }
         }
       }
     }
