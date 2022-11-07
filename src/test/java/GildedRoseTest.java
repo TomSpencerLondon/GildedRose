8596af820ede9071bfdbc8e74e81e9477df9c379
Tom Spencer <thomas.spencer@codurance.com>
2022-11-07 19:06:03 +0000

    Add full coverage for acceptance test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -18,6 +18,19 @@ public class GildedRoseTest {
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 		items.add(new Item("Elixir of the Mongoose", 1, -1));
+		items.add(new Item("Aged Brie", 1, 51));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 51));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 48));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47));
+		items.add(new Item("+5 Dexterity Vest", -1, 1));
+		items.add(new Item("+5 Dexterity Vest", -1, 3));
+		items.add(new Item("Aged Brie", -1, 3));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", -1, 3));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", -1, 3));
+		items.add(new Item("Aged Brie", -1, 51));
 		List<String> result = new ArrayList<>();
 		GildedRose.updateQuality(items);
 		for (Item item : items) {
