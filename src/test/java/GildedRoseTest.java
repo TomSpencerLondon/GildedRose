ede7f5dc8f338c1349a4e621ba43f66dba0c0b20
Tom Spencer <thomas.spencer@codurance.com>
2022-11-07 18:15:04 +0000

    Add approval test for GildedRose

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,12 +1,32 @@
-import static org.junit.Assert.*;
+import static org.approvaltests.Approvals.verifyAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import org.approvaltests.strings.Printable;
+import org.junit.jupiter.api.Test;
 
 
 public class GildedRoseTest {
 
 	@Test
 	public void testTheTruth() {
-		assertTrue(true);
+		List<Item> items = new ArrayList<>();
+		items.add(new Item("+5 Dexterity Vest", 10, 20));
+		items.add(new Item("Aged Brie", 2, 0));
+		items.add(new Item("Elixir of the Mongoose", 5, 7));
+		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+		items.add(new Item("Conjured Mana Cake", 3, 6));
+		List<String> result = new ArrayList<>();
+		GildedRose.updateQuality(items);
+		for (Item item : items) {
+			result.add(String.format("Name: %s, SellIn: %d, Quality: %d",
+					item.getName(),
+					item.getSellIn(),
+					item.getQuality()));
+		}
+
+		verifyAll("", result);
 	}
 }
